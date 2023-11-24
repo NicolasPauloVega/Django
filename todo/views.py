3288c4ebbd8da42423005d7be71ad94dfe2aab4a
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2023-11-24 08:48:05 -0500

    A1

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Tarea
 # Create your views here.
+
+def home(request):
+    tarea = Tarea.objects.all()
+    context = {'tareas': tarea}
+    return render(request, 'home.html',context)
