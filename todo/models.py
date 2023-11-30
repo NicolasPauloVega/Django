27d5aa75d154b9c0e3ac034d53ddd7e73dc98c07
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2023-11-30 09:32:48 -0500

    A1

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Tarea(models.Model):
-    tarea=models.CharField(max_length=100)
+    tareas=models.CharField(max_length=100)
     def __str__(self):
-        return self.tarea
+        return self.tareas
 
 # Create your models here.
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Tarea
 # Create your views here.
 def home(request):
-    tarea = Tarea.objects.all()
-    context = {'tareas': tarea}
+    tareas = Tarea.objects.all()
+    context = {'tareas': tareas}
     return render(request, 'todo/home.html',context)
