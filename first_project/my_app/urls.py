2620e27d8d7e9bed32901adf3060d548b4f49174
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-03 16:04:23 -0500

    urls (mi_app)
    arreglo de links de app

--- first_project/my_app/urls.py
+++ first_project/my_app/urls.py
@@ -17,31 +17,31 @@ Including another URLconf
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-import miApp.views
+from .views import *
 
 urlpatterns = [
-    path('holaMundo/', miApp.views.holaMundo, name="holaMundo"),
-    path('saludo/<int:redirigir>', miApp.views.saludo, name="saludo"),
-    path('', miApp.views.index, name="index"),
-    path('presentacion/', miApp.views.presentacion, name="presentacion"),
-    path('contacto/', miApp.views.contacto, name="contacto"),
-    path('contacto/<str:nombre>', miApp.views.contacto, name="contacto"),
-    path('contacto/<str:nombre>/<str:apellido>', miApp.views.contacto, name="contacto"),
-    path('quienesSomos/', miApp.views.quienesSomos, name="QuienesSomos"),
-    path('productosServicios/', miApp.views.productosServicios, name="ProductosServicios"),
-    path('ejercicioBucle/', miApp.views.ejercicioBucle, name="ejercicioBucle"),
-    path('pagina/', miApp.views.pagina, name="Página"),
-    path('crear_articulo/', miApp.views.crear_articulo, name="Crear_articulo"),
-    path('crear_articulo/<str:title>/<str:content>/<str:public>', miApp.views.crear_articulo, name="crear_articulo"),
-    path('articulo/', miApp.views.articulo, name="Articulo"),
-    path('editar_articulo/<int:id>', miApp.views.editar_articulo, name="Editar_Articulo"),
-    path('articulos/', miApp.views.articulos, name="Listar"),
-    path('borrar_articulo/<int:id>', miApp.views.borrar_articulo, name="Borrar"),
-    path('delete_articulos/<int:id>', miApp.views.delete_articulos, name="delete_articulos"),
-    path('update_articulo/<str:title>/<int:id>', miApp.views.update_articulo, name="Actualiazar SQL"),
-    path('create_articulo/', miApp.views.create_articulo, name="Create"),
-    path('save_articulo/', miApp.views.save_articulo, name="Save"),
-    path('create-full-article/', miApp.views.create_full_articulo, name="create_full"),
+    path('holaMundo/', holaMundo, name="holaMundo"),
+    path('saludo/<int:redirigir>', saludo, name="saludo"),
+    path('', index, name="index"),
+    path('presentacion/', presentacion, name="presentacion"),
+    path('contacto/', contacto, name="contacto"),
+    path('contacto/<str:nombre>', contacto, name="contacto"),
+    path('contacto/<str:nombre>/<str:apellido>', contacto, name="contacto"),
+    path('quienesSomos/', quienesSomos, name="QuienesSomos"),
+    path('productosServicios/', productosServicios, name="ProductosServicios"),
+    path('ejercicioBucle/', ejercicioBucle, name="ejercicioBucle"),
+    path('pagina/', pagina, name="Página"),
+    path('crear_articulo/', crear_articulo, name="Crear_articulo"),
+    path('crear_articulo/<str:title>/<str:content>/<str:public>', crear_articulo, name="crear_articulo"),
+    path('articulo/', articulo, name="Articulo"),
+    path('editar_articulo/<int:id>', editar_articulo, name="Editar_Articulo"),
+    path('articulos/', articulos, name="Listar"),
+    path('borrar_articulo/<int:id>', borrar_articulo, name="Borrar"),
+    path('delete_articulos/<int:id>', delete_articulos, name="delete_articulos"),
+    path('update_articulo/<str:title>/<int:id>', update_articulo, name="Actualiazar SQL"),
+    path('create_articulo/', create_articulo, name="Create"),
+    path('save_articulo/', save_articulo, name="Save"),
+    path('create-full-article/', create_full_articulo, name="create_full"),
 ]
 
 if settings.DEBUG:
