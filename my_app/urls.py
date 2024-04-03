0c75d0247b742a338fac0adb60965452074a73f7
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-03 17:28:00 -0500

    views and urls
    vistas y url para el diseño de la pagina

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,5 +1,25 @@
 from django.urls import path
 
+#Importamos las vistas en la url
+from .views import *
+
 urlpatterns = [
-    
+    #links para el inicio
+    path('', index, name="start"),
+    path('index/', index, name="index"),
+
+    #links para hola mundo
+    path('hello-world/', hello_world, name="hello_world"),
+
+    #links para la pgina del sitio web
+    path('test-page/', page, name="pages"),
+    path('test-page/<int:redirects>', page, name="pages"),
+
+    #links para la parte de la bienvenida
+    path('welcome/', welcome, name="welcome"),
+
+    #links para el contacto
+    path('contact/', contact, name="contact"),
+    path('contact/<str:name>', contact, name="contact"),
+    path('contact/<str:name>/<str:last_name>', contact, name="contact"),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,62 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
-def hola_mundo(request):
-    return HttpResponse("Hola mundo con Django!!")
+layout ="""
+            <h1>Sitio Web con Django | Nicolas Paulo Vega</h1>
+            <hr>
+            <ul>
+                <li>
+                    <a href="/welcome">Bienvenido</a>
+                </li>
+                <li>
+                    <a href="/index">Inicio</a>
+                </li>
+                <li>
+                    <a href="/hello-world">Hola mundo</a>
+                </li>
+                <li>
+                    <a href="/test-page">Página</a>
+                </li>
+            </ul>
+        """
+
+def hello_world(request):
+    return HttpResponse(
+        """
+            <h1>Hola mundo</h1>
+            <h3>Sena todos ustedes bienvenidos!!</h3>
+        """
+    )
+
+def index(request):
+    template = """
+                    <h1>Inicio</h1>
+                    <p>Años hasta el 2050</p>
+                    <ul>
+                """
+    year = 2024
+    while year <= 2050:
+        template += f"<li> {str(year)} </li>"
+        year += 1
+    return HttpResponse(layout+template)
+
+def page(request, redirects=0):
+    if redirects==1:
+        return redirect('contact', name="Ana", last_name="Perez")
+    return HttpResponse(
+        """
+            <h1>Pagina de mi web</h1>
+            <p>Creado por Nicolas Paulo Vega</p>
+        """
+    )
+
+def welcome(request):
+    return HttpResponse('Hola Mundo desde Django')
+
+def contact(request, name="", last_name=""):
+    html = ""
+    if name and last_name:
+        html = "<h2>Nombre Completo: </h2>"
+        html += f"<h3> {name} {last_name} </h3>"
+    return HttpResponse(layout+f"<h2>Contacto </h2>"+html)
+
