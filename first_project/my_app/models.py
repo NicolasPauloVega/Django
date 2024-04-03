896eb622d121c7f208e91dc6eae209e4a7c99324
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-03 16:09:13 -0500

    settings
    configurar el proyecto

--- first_project/my_app/models.py
+++ first_project/my_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(default='null', upload_to="articles")
+    public = models.BooleanField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Articulo"
+        verbose_name_plural = "Articulos"
+        ordering = ['id']
+
+    def __str__(self):
+        if self.public:
+            publico = "(Publico)"
+        else:
+            publico = "(Privado)"
+        return f"{self.id}-{self.title} : {publico}"
