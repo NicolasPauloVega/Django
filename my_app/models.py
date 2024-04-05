39c6f0de5dd9e277fdd029ebd65192df34b37dbd
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-04 20:06:54 -0500

    article and category models y sweetalert

--- my_app/models.py
+++ my_app/models.py
@@ -9,7 +9,49 @@ class Article(models.Model):
     create_date = models.DateTimeField(auto_now_add = True)
     update_date = models.DateField(auto_now = True)
 
+    @classmethod
+    def get_next_code(cls):
+        # Método para obtener el próximo código disponible sumando 1 al código del último registro
+        # Ordenamos los registros por código de manera descendente y obtenemos el primer registro
+        last_record = cls.objects.order_by('-code').first()
+        if last_record:
+            # Si hay registros, sumamos 1 al código del último registro
+            return last_record.code + 1
+        else:
+            # Si no hay registros, el próximo código será 1
+            return 1
+
+    def save(self, *args, **kwargs):
+        # Método para guardar el objeto Materi en la base de datos
+        if not self.code:
+            # Verificamos si el campo code ya tiene un valor asignado
+            # Si no tiene un valor (es decir, es None o no está definido), asignamos el próximo código disponible
+            self.code = self.get_next_code()
+        # Llamamos al método save de la clase padre para guardar el objeto en la base de datos
+        super().save(*args, **kwargs)
+
 class Category(models.Model):
     name = models.CharField(max_length=110)
     description = models.CharField(max_length=250)
     create_date = models.DateField()
+
+    @classmethod
+    def get_next_code(cls):
+        # Método para obtener el próximo código disponible sumando 1 al código del último registro
+        # Ordenamos los registros por código de manera descendente y obtenemos el primer registro
+        last_record = cls.objects.order_by('-code').first()
+        if last_record:
+            # Si hay registros, sumamos 1 al código del último registro
+            return last_record.code + 1
+        else:
+            # Si no hay registros, el próximo código será 1
+            return 1
+
+    def save(self, *args, **kwargs):
+        # Método para guardar el objeto Materi en la base de datos
+        if not self.code:
+            # Verificamos si el campo code ya tiene un valor asignado
+            # Si no tiene un valor (es decir, es None o no está definido), asignamos el próximo código disponible
+            self.code = self.get_next_code()
+        # Llamamos al método save de la clase padre para guardar el objeto en la base de datos
+        super().save(*args, **kwargs)
